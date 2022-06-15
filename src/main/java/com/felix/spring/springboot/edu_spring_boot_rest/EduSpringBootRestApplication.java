ccd629197e1f4bedee9d88a514675e154ecd4c77
felix <Felix0417@github.com>
2022-06-16 00:20:37 +0300

    Let's go!

---
+++ src/main/java/com/felix/spring/springboot/edu_spring_boot_rest/EduSpringBootRestApplication.java
@@ -0,0 +1,13 @@
+package com.felix.spring.springboot.edu_spring_boot_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduSpringBootRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduSpringBootRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/felix/spring/springboot/edu_spring_boot_rest/EduSpringBootRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felix.spring.springboot.edu_spring_boot_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduSpringBootRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

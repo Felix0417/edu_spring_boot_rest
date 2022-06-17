b288f4f6d47df16c30ece1374083a972c06eca2e
felix <Felix0417@github.com>
2022-06-18 00:22:28 +0300

    Added new dependency and uncommented hide codes

--- src/main/java/com/felix/spring/springboot/edu_spring_boot_rest/dao/EmployeeDAOImpl.java
+++ src/main/java/com/felix/spring/springboot/edu_spring_boot_rest/dao/EmployeeDAOImpl.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 import java.util.List;
 
 @Repository
@@ -24,24 +25,24 @@ public class EmployeeDAOImpl implements EmployeeDAO{
         return allEmployees;
     }
 
-//    @Override
-//    public void saveEmployee(Employee employee) {
-//        sessionFactory.getCurrentSession().saveOrUpdate(employee);
-//    }
-//
-//    @Override
-//    public Employee getEmployee(int id) {
-//        Session session = sessionFactory.getCurrentSession();
-//        Employee employee = session.get(Employee.class, id);
-//        return employee;
-//    }
-//
-//    @Override
-//    public void deleteEmployee(int id) {
-//        Session session = sessionFactory.getCurrentSession();
-//        Query<Employee> query = session.createQuery("delete from Employee "+
-//                "where id =:employeeId");
-//        query.setParameter("employeeId", id);
-//        query.executeUpdate();
-//    }
+    @Override
+    public void saveEmployee(Employee employee) {
+        entityManager.unwrap(Session.class).saveOrUpdate(employee);
+    }
+
+    @Override
+    public Employee getEmployee(int id) {
+        Session session = entityManager.unwrap(Session.class);
+        Employee employee = session.get(Employee.class, id);
+        return employee;
+    }
+
+    @Override
+    public void deleteEmployee(int id) {
+        Session session = entityManager.unwrap(Session.class);
+        Query query = session.createQuery("delete from Employee "+
+                "where id =:employeeId");
+        query.setParameter("employeeId", id);
+        query.executeUpdate();
+    }
 }
